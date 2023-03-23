0e1128c03e622423fa141bdb3993db3d2d10f312
myUsername <myemail@gmail.com>
2023-03-23 05:12:44 -0700

    0 > 972?

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -9,27 +9,27 @@
  */
 
 int largest_number(int a, int b, int c)
-
 {
 	int largest;
-	
-	if (a > b && a > c)
+
+	if (a > b && b > c)
 	{
 		largest = a;
 	}
-	else if (a > b && c > a)
-	{
-		largest = c;
-	}
-	else if (b > c)
+	else if (a > b && c > b)
+		largest = a;
+	else if (b > a && a > c)
 	{
 		largest = b;
 	}
+	else if (b > c && c > a)
+		largest = b;
+	else if (c > a && a > b)
+		largest = c;
 	else
 	{
 		largest = c;
 	}
-	
+
 	return (largest);
 }
-
