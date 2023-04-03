327717c3fac5cfc840f2ac38abe35a7c229fa460
BangbangOG <letsoenyothabang@gmail.com>
2023-04-03 08:56:59 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,20 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <unistd.h>
 /**
-*main - Entry point
-*
-Return: Always 0 (Success)
-*/
-int main(void)
-
-
-#include<stdio.h>
-/**
-*main - Entry point
-*
-Return: Always 0 (Success)
-*/
+ * main - A c program that prints a line to the standard error
+ *
+ * Return: 1 (Success)
+ */
 int main(void)
 {
-	write(2,"and that piece of art is useful"\" - Dora Korpar, 2015-10-19\n", 59):
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\r", stdout);
 	return (1);
 }
