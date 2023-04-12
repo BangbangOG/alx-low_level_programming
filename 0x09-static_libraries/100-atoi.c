f66073b4e29080a18c5d2b1ae4d8bc6987c9a07f
BangbangOG <letsoenyothabang@gmail.com>
2023-04-12 16:38:27 -0700

    libmy.a

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,18 +1,9 @@
-#include "main.h"
-/**
- * _strchr - Entry point
- * @s: input
- * @c: input
- * Return: Always 0 (Success)
- */
-char *_strchr(char *s, char c)
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+int _atoi(char *s)
+
 {
-	int i = 0;
-	
-	for (; s[i] >= '\0'; i++)
-	{
-		if (s[i] == c)
-			return (&s[i]);
-	}
-	return (0);
+	return atoi(s);
 }
