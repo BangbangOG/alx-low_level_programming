05c3feed755118c77419d240d4914b5357a9a6a4
BangbangOG <letsoenyothabang@gmail.com>
2023-04-28 06:34:37 -0700

    4-free_list.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -5,7 +5,7 @@
  * @s1: pointer to string param
  * @s2: pointer to string params
  *
- * Return: 0 (Success)
+ * Return: 0
  */
 int wildcmp(char *s1, char *s2)
 {
@@ -13,11 +13,11 @@ int wildcmp(char *s1, char *s2)
 	{
 		if (*s2 != '\0' && *s2 == '*')
 		{
-			return (wildcmp(s1, s2, + 1));
+			return (wildcmp(s1, s2, +1));
 		}
 		return (*s2 == '\0');
 	}
-	if (*s2 == '*')
+		if (*s2 == '*')
 	{
 		 return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 	}
